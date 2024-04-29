ddfd646f46a4e968e94816ee9479d721394260a5
luzhipeng728 <luzhipeng728@126.com>
2024-04-29 15:37:40 +0800

    更新了框架初始话的一些状态

--- uptime.config.ts
+++ uptime.config.ts
@@ -74,7 +74,7 @@ const config = {
       // `id` 应该是唯一的，如果 `id` 保持不变，历史记录将被保留
       id: 'channel-1',
       // `name` 在状态页面和回调消息中使用
-      name: 'Channen-1 gpt-4-turbo',
+      name: 'channel-1 gpt-4-turbo',
       // `method` 应该是有效的 HTTP 方法
       method: 'POST',
       // `target` 是有效的 URL
